6a4f1794c52653f3b48730ad62a5e31137f418bd
vallivetik <vallivetikishorekumarreddy@gmail.com>
2023-08-09 12:48:26 +0530

    implementing login and logout

--- liberarymsystem/src/app/app.module.ts
+++ liberarymsystem/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -9,6 +9,8 @@ import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
 import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 import { LoginComponent } from './login/login.component';
 import { AboutpageComponent } from './aboutpage/aboutpage.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ErrorComponent } from './error/error.component';
 
 
 @NgModule({
@@ -18,12 +20,15 @@ import { AboutpageComponent } from './aboutpage/aboutpage.component';
     HeaderNavbarComponent,
     SideNavbarComponent,
     LoginComponent,
-    AboutpageComponent
+    AboutpageComponent,
+    LogoutComponent,
+    ErrorComponent
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
 
   ],
   providers: [],
