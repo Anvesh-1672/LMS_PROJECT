c01e6b32f47148b62a499036c4e9598b5991a648
vallivetik <vallivetikishorekumarreddy@gmail.com>
2023-08-09 10:53:22 +0530

    routing path is clear

--- liberarymsystem/src/app/app-routing.module.ts
+++ liberarymsystem/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
 
 const routes: Routes = [
-  {}
+  {path:'',component:HomePageComponent},
+  {path:'home',component:HomePageComponent},
+  {path:'about',component:AboutpageComponent},
+  {path:'login',component:LoginComponent}
 
 ];
 
