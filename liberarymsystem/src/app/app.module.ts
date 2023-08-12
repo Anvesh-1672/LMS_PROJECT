98fc8c63f9bb5da528dbdee0cff22a58388b8c96
vallivetik <vallivetikishorekumarreddy@gmail.com>
2023-08-12 12:33:27 +0530

    profile

--- liberarymsystem/src/app/app.module.ts
+++ liberarymsystem/src/app/app.module.ts
@@ -6,12 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
-import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 import { LoginComponent } from './login/login.component';
 import { AboutpageComponent } from './aboutpage/aboutpage.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ErrorComponent } from './error/error.component';
 import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 @NgModule({
@@ -19,12 +19,12 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     HomePageComponent,
     HeaderNavbarComponent,
-    SideNavbarComponent,
     LoginComponent,
     AboutpageComponent,
     LogoutComponent,
     ErrorComponent,
-    RegisterComponent
+    RegisterComponent,
+    UserProfileComponent,
    
   ],
   imports: [
