3d1e3ac90e54aa7b181f8ad9e6c0273157b981ac
Anvesh-1672 <kunchamanvesh6@gmail.com>
2023-10-11 13:47:06 +0530

    AfterlongAnvesh

---
+++ liberarymsystem/src/app/ad-dashboard/ad-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-dashboard',
+  templateUrl: './ad-dashboard.component.html',
+  styleUrls: ['./ad-dashboard.component.css']
+})
+export class AdDashboardComponent {
+
+}
--- liberarymsystem/src/app/app-routing.module.ts
+++ liberarymsystem/src/app/app-routing.module.ts
@@ -8,12 +8,14 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorComponent } from './error/error.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { RouteGuardService } from './service/route-gaurd.service';
+import { AdDashboardComponent } from './ad-dashboard/ad-dashboard.component';
 
 const routes: Routes = [
   {path:'',component:HomePageComponent},
   {path:'home', component:HomePageComponent},
   {path:'login',component:LoginComponent},
   {path:'about',component:AboutpageComponent},
+  {path:'dashboard',component:AdDashboardComponent},
   {path:'logout',component:LogoutComponent,canActivate:[RouteGuardService]},
   {path:'register',component:RegisterComponent},
   {path:'profile',component:UserProfileComponent,canActivate:[RouteGuardService]},
